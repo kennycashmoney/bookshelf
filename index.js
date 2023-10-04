4cc27eeb36a12cdd32f9f2e3b6b7b4e97ccf78de
kennycashmoney <ken.larose@proton.me>
2023-10-04 14:25:25 -0500

    added swagger docs UI (no services added yet)

--- index.js
+++ index.js
@@ -1,9 +1,27 @@
 const express = require("express");
+const swaggerUI = require('swagger-ui-express');
+const swaggerJsDoc = require('swagger-jsdoc');
 
 const booksRouter = require("./routes/books.router");
 
 const app = express();
 const PORT = 3000;
+
+/** Swagger Docs */
+const options = {
+    definition: {
+        openapi: "3.0.0",
+        info: {
+            title: "Bookshelf",
+            version: "1.0.0",
+            description: "a RESTful API representation of the bookshelf in my office"
+        }
+    },
+    apis: ["./routes/*.js"],
+};
+const specs = swaggerJsDoc(options);
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(specs));
+
 
 /** Logging Middleware */
 app.use((req, _res, next) => {
@@ -13,10 +31,11 @@ app.use((req, _res, next) => {
   console.log(`${req.method} ${req.baseUrl}${req.url} ${delta}ms`);
 });
 
+/** API Router */
 app.use(express.json()); // parse the request body to JSON object
 app.use("/books", booksRouter);
 
-/** error handling */
+/** Error Handling */
 app.use((_req, res) => res.status(404).send("Sorry can't find that!"));
 app.use((err, _req, res, _next) => {
   console.error(err.stack);
